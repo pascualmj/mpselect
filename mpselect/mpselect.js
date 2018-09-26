15042f0b3219eb2ffe9ca54e0f733d3c7bfb9389
Manuel Pascual <manupascual90@gmail.com>
2018-09-26 17:35:28 -0300

    Create the element for the current item

--- mpselect/mpselect.js
+++ mpselect/mpselect.js
@@ -4,10 +4,10 @@
 	$.fn.mpselect = function() {
 
 		// First hide the <select> element
-		this.children('select').css('visibility', 'hidden');
+		this.children('select').prop('hidden', true);
 
 		// Create a list with the options select tag has
-		this.append('<ul class="mpoptions"></ul>');
+		this.prepend('<ul class="mpoptions"></ul>');
 		this.find('select option').each(function() {
 			var mpselect_option_value = $(this).val(),
 				mpselect_option_text = $(this).text(),
@@ -16,6 +16,9 @@
 				$(this).closest('.mpselect').find('ul.mpoptions').append('<li data-value="' + mpselect_option_format[0] + '">' + mpselect_option_format[1] + '</li>');
 			}
 		});
+
+		// Create the element for the current item
+		this.prepend('<span class="mpcurrent">' + this.find('option[value=0]').text() + '</span>');
 
 		// Return the element for chaining functionality
 		return this;
