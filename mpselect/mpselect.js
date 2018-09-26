401708b6cdde98e1cda1685068a0ade6fac2b1ef
Manuel Pascual <manupascual90@gmail.com>
2018-09-26 17:51:47 -0300

    Add click event for list options

--- mpselect/mpselect.js
+++ mpselect/mpselect.js
@@ -20,6 +20,15 @@
 		// Create the element for the current item
 		this.prepend('<span class="mpcurrent">' + this.find('option[value=0]').text() + '</span>');
 
+		// Click event for list options
+		this.find('ul.mpoptions li').on('click', function() {
+			var mpselect_mpoption_value = $(this).data('value'),
+				mpselect_mpoption_text = $(this).text();
+			$(this).addClass('mpactive').siblings('li').removeClass('mpactive');
+			$(this).closest('.mpselect').find('select option[value=' + mpselect_mpoption_value + ']').prop('selected', true);
+			$(this).closest('.mpselect').find('span.mpcurrent').text(mpselect_mpoption_text);
+		});
+
 		// Return the element for chaining functionality
 		return this;
 	};
